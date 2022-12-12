1e9ea398acd0814788a135de608bc28a9790be5f
Tom Faabry <110841289+Faabry@users.noreply.github.com>
2022-12-12 18:27:11 -0300

    Update Todo List

--- todo_list.py
+++ todo_list.py
@@ -1,25 +1,25 @@
 import PySimpleGUI as sg
 
 def criando_janela():
-    sg.theme_background_color('#696969')
+    sg.theme_background_color('#303030')
     linha = [
         # Criando uma linha inicial que vai ser uma tarefa individual
-        [sg.Checkbox('', background_color='#696969', text_color='#FFA500'),
+        [sg.Checkbox('', background_color='#303030', text_color='#FFA500'),
          sg.Input(font='Times 15')]
     ]
     
     # Criando o layout principal do programa
     layout = [
         [sg.Text('Todo List'.center(43), font='Times 30 bold', text_color='#FFA500',
-         background_color='#696969')],
-        [sg.Frame('Tarefas', layout= linha, background_color='#696969',
+         background_color='#303030')],
+        [sg.Frame('Tarefas', layout= linha, background_color='#303030',
          title_color='#FFA500', font='Times 20 bold', key='-container-')],
         [sg.Button('Nova Tarefa', font='Times 13', button_color='black on #FFA500'),
          sg.Button('Zerar', font='Times 13', button_color='black on #FFA500')]
     ]
 
     return sg.Window('Developed by Airton Fabre', layout=layout,
-                     finalize=True, background_color='#696969', location=(400, 100))
+                     finalize=True, background_color='#303030', location=(400, 100))
 
 
 # ---------- Programa Principal -------------
@@ -37,7 +37,7 @@ while True:
     
         # janela na posição -container- vai receber um novo checkbox mais o input
         window.extend_layout(window['-container-'],
-         [[sg.Checkbox('', background_color='#696969', text_color='#FFA500'),
+         [[sg.Checkbox('', background_color='#303030', text_color='#FFA500'),
          sg.Input(font='Times 15')]])
 
         # Zerando as tarefas e reiniciando o programa
